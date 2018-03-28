bb20954531521c46bbd8d3775fb087b5bcb63217
Paul Morea <paul_morea@hotmail.fr>
2018-03-28 12:28:47 +0200

    implement gamepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { GamepageComponent } from './gamepage/gamepage.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamepageComponent
   ],
   imports: [
     BrowserModule
